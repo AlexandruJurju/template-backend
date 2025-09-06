cef26ac4a9665a25f0dd1d5033cb1c15651ba62e
AlexandruJurju <alexandru.i.jurju@gmail.com>
2025-09-06 12:45:37 +0300

    YARP

--- Template.YARP/Program.cs
+++ Template.YARP/Program.cs
@@ -1,7 +1,10 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 WebApplication app = builder.Build();
 
-app.UseHttpsRedirection();
+app.MapReverseProxy();
 
 await app.RunAsync();
